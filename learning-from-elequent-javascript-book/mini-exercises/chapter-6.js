b5741e872dce297b32385e2085cbd02fdf3b2c0d
dzonasm <jmarciulevicius@gmail.com>
2021-02-18 10:23:18 +0200

    completed the debugging chapter excercises

---
+++ learning-from-elequent-javascript-book/mini-exercises/chapter-6.js
@@ -0,0 +1,64 @@
+// A vector type
+// Write a class Vec that represents a vector in two-dimensional space. It takes
+// x and y parameters (numbers), which it should save to properties of the same
+// name.
+// Give the Vec prototype two methods, plus and minus, that take another
+// vector as a parameter and return a new vector that has the sum or difference
+// of the two vectors’ (this and the parameter) x and y values.
+// Add a getter property length to the prototype that computes the length of
+// the vector—that is, the distance of the point (x, y) from the origin (0, 0).
+
+
+class Vec {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    plus(otherVec) {
+        return { x: this.x + otherVec.x, y: this.y + otherVec.y }
+    }
+    minus(otherVec) {
+        return { x: this.x - otherVec.x, y: this.y - otherVec.y }
+    }
+}
+
+// const myVec = new Vec(1, 2);
+// const yourVec = new Vec(1, 0);
+
+// console.log(myVec.plus(yourVec));
+// console.log(myVec.minus(yourVec));
+
+// Groups
+// The standard JavaScript environment provides another data structure called
+// Set. Like an instance of Map, a set holds a collection of values. Unlike Map, it
+// does not associate other values with those—it just tracks which values are part
+// of the set. A value can be part of a set only once—adding it again doesn’t have
+// any effect.
+// Write a class called Group (since Set is already taken). Like Set, it has add,
+// delete, and has methods. Its constructor creates an empty group, add adds
+// a value to the group (but only if it isn’t already a member), delete removes
+// its argument from the group (if it was a member), and has returns a Boolean
+// value indicating whether its argument is a member of the group.
+// Use the === operator, or something equivalent such as indexOf, to determine
+// whether two values are the same.
+// Give the class a static from method that takes an iterable object as argument
+// and creates a group that contains all the values produced by iterating over it.
+
+class Group {
+    constructor() {
+        this.group = [];
+    }
+    add(value) {
+        if (!this.group.has(value)) {
+            return this.group.push(value);
+        }
+    }
+    delete(value) {
+        this.group = this.group.filter(item => item !== value);
+    }
+    has(value) {
+        return this.group.includes(value);
+    }
+}
+
+
---
+++ learning-from-elequent-javascript-book/mini-exercises/chapter-8.js
@@ -0,0 +1,74 @@
+// Retry
+// Say you have a function primitiveMultiply that in 20 percent of cases multiplies two numbers and in the other 80 percent of cases raises an exception
+// of type MultiplicatorUnitFailure. Write a function that wraps this clunky
+// function and just keeps trying until a call succeeds, after which it returns the
+// result.
+// // Make sure you handle only the exceptions you are trying to handle.
+
+const primitiveMultiply = () => {
+    let randomNum = Math.floor(Math.random() * 5);
+    if (randomNum === 3) {
+        return "success";
+    }
+    return "failed";
+}
+
+const loopUntilSuccess = (func) => {
+    for (; ;) {
+        if (func() === "success") return "got it";
+    }
+}
+
+// console.log(loopUntilSuccess(primitiveMultiply));
+
+
+// The locked box
+// Consider the following (rather contrived) object:
+// const box = {
+// locked: true,
+// unlock() { this.locked = false; },
+// lock() { this.locked = true; },
+// _content: [],
+// get content() {
+// if (this.locked) throw new Error("Locked!");
+// return this._content;
+// }
+// };
+// It is a box with a lock. There is an array in the box, but you can get at it only
+// when the box is unlocked. Directly accessing the private _content property is
+// forbidden.
+// Write a function called withBoxUnlocked that takes a function value as argument, unlocks the box, runs the function, and then ensures that the box
+// is locked again before returning, regardless of whether the argument function
+// returned normally or threw an exception.
+// For extra points, make sure that if you call withBoxUnlocked when the box
+// is already unlocked, the box stays unlocked.
+
+const box = {
+    locked: true,
+    unlock() { this.locked = false; },
+    lock() { this.locked = true; },
+    _content: [],
+    get content() {
+        if (this.locked) throw new Error("Locked!");
+        return this._content;
+    }
+};
+
+const withBoxUnlocked = (func) => {
+    let locked = box.locked;
+    if (!locked) {
+        return func();
+    } else {
+        box.unlock();
+        func();
+        box.lock();
+    }
+}
+
+const leaveNote = () => {
+    console.log(box.locked)
+    box.content.push('Jonas was here!')
+}
+
+// withBoxUnlocked(leaveNote);
+// console.log(box);
